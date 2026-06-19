2eaf29c4d272d03a8c7aeee2c3b07460920effe6
DanielPuca <Daniel_puk95@fi.unju.edu.ar>
2026-06-19 17:20:33 -0300

    Agregue data/usuarios.js y actualize el Login.jsx- Daniel Puca

---
+++ src/data/usuarios.js
@@ -0,0 +1,26 @@
+
+const usuarios = [
+    { 
+        id: 1, 
+        nombre: "Daniel Puca", 
+        usuario: "Dapuca", 
+        contrasena: "Dan1234", 
+        rol: "Gerencia" 
+    },   
+    { 
+        id: 2, 
+        nombre: "Antonella Csongedy", 
+        usuario: "Ancsongedy", 
+        contrasena: "Ant1234", 
+        rol: "Soporte" 
+    },
+    { 
+        id: 3, 
+        nombre: "Lucas Velasquez", 
+        usuario: "Luvelasquez", 
+        contrasena: "Luc1234", 
+        rol: "Soporte" 
+    }
+    ];
+
+export default usuarios;
--- src/services/adminServices.js
+++ src/services/adminServices.js
@@ -1,18 +1,12 @@
-
+import usuarios from '../data/usuarios';
 
 const adminService = (() => {
-
-    const admins = [
-        { id: 1, nombre: "Daniel Puca", usuario: "Dapuca", contrasena: "Dan1234", rol: "Gerencia" },
-        { id: 2, nombre: "Antonella Csongedy", usuario: "Ancsongedy", contrasena: "Ant1234", rol: "Soporte" },
-        { id: 3, nombre: "Lucas Velasquez", usuario: "Luvelasquez", contrasena: "Luc1234", rol: "Soporte" }
-    ];
 
     const validarAdmin = (usuario, contrasena) => {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                const encontrado = admins.find(
-                    (a) => a.usuario === usuario && a.contrasena === contrasena
+                const encontrado = usuarios.find(
+                    (u) => u.usuario === usuario && u.contrasena === contrasena
                 );
                 if (encontrado) {
                     resolve(encontrado);
